891b9afee314756db145675584fcd4ec1c2ec8be
Ioannis Antoniou <johnantony24@hotmail.com>
2022-11-22 17:22:33 +0100

    Profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-
+  get '/profile', to: 'pages#profile'
+  patch '/profile', to: 'pages#update_profile'
   resources :listings do
   end
 end
