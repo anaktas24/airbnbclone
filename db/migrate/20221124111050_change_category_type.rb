82a5f058ddcc95fdbb2c293d6390095d482199ac
omarwagon <115075310+omarwagon@users.noreply.github.com>
2022-11-24 16:36:32 +0100

    Fix Filtering
    Fixes everything

--- db/migrate/20221124111050_change_category_type.rb
+++ db/migrate/20221124111050_change_category_type.rb
@@ -2,6 +2,6 @@ class ChangeCategoryType < ActiveRecord::Migration[7.0]
   # enable_extension "plpsql"
   create_enum "category", ["Electronic", "Manual", "Gas-powered", "Other"]
   def change
-    add_column :listings, :category_type, :enum, null: false, enum_type: "category", default: ""
+    add_column :listings, :category_type, :enum, null: false, enum_type: "category", default: "Other"
   end
 end
