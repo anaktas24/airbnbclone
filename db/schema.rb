96c10434aeb0a24b6580c83896908d14c03d4bab
marlin coroban <marlin.coroban@gmail.com>
2022-11-25 12:27:23 +0100

    buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_125306) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "bookings", force: :cascade do |t|
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.bigint "listing_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status", default: "pending"
-    t.index ["listing_id"], name: "index_bookings_on_listing_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_113305) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_142608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
-  create_enum "cat", ["Electronic", "Manual", "Gas-powered", "Other"]
   create_enum "category", ["Electronic", "Manual", "Gas-powered", "Other"]
-  create_enum "categoryList", ["Electronic", "Manual", "Gas-powered", "Other"]
-  create_enum "status", ["Electronic", "Manual", "Gas-powered", "Other"]
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_142608) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -67,7 +44,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_142608) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.bigint "listing_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
+    t.index ["listing_id"], name: "index_bookings_on_listing_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -79,7 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_142608) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "latitude"
-    t.float "longitude" 
+    t.float "longitude"
     t.enum "category_type", default: "Other", null: false, enum_type: "category"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
@@ -101,11 +89,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_142608) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookings", "listings"
-  add_foreign_key "bookings", "users"
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
+  add_foreign_key "bookings", "listings"
+  add_foreign_key "bookings", "users"
   add_foreign_key "listings", "users"
 end
