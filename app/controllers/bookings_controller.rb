c7e8d59fbfc5a1fdf2afb8807084cc7f8591e902
Ioannis Antoniou <johnantony24@hotmail.com>
2022-11-25 14:31:04 +0100

    fix btn on booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -65,6 +65,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :photo)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
